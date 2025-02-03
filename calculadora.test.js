e1888e829287a7dba3e9da84e5299815afa550e0
MarinaRT2003 <mreptru297@g.educaand.es>
2025-02-03 11:10:47 +0100

    Nuevas operaciones add

--- calculadora.test.js
+++ calculadora.test.js
@@ -7,3 +7,15 @@ test('Suma 2 + 3 y devuelve 5', () => {
 test('Resta 5 - 3 y devuelve 2', () => {
     expect(restar(5, 3)).toBe(2);
 });
+
+test('Multiplicar 2 * 3 y devuelve 6', () => {
+    expect(multiplicar(2, 3)).toBe(6);
+});
+
+test('Dividir 6 / 3 y devuelve 2', () => {
+    expect(dividir(6, 3)).toBe(2);
+});
+
+test('Raiz cuadrada de 25 y devuelve 5', () => {
+    expect(raizCuadrada(25)).toBe(5);
+});
