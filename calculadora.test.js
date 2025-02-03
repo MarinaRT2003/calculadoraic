6f7715f18ba19f91cefd8b537531624c3a82828d
MarinaRT2003 <mreptru297@g.educaand.es>
2025-02-03 11:14:03 +0100

    Pruebas unitarias suma y resta

--- calculadora.test.js
+++ calculadora.test.js
@@ -19,3 +19,11 @@ test('Dividir 6 / 3 y devuelve 2', () => {
 test('Raiz cuadrada de 25 y devuelve 5', () => {
     expect(raizCuadrada(25)).toBe(5);
 });
+
+test('Suma 7 + 3 y devuelve 10', () => {
+    expect(sumar(7, 3)).toBe(10);
+});
+
+test('Resta 10 - 3 y devuelve 7', () => {
+    expect(restar(10, 3)).toBe(7);
+});
