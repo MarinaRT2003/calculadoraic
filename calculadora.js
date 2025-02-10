bdf2535a8df6e5458e006113d7fe738d9c86e9e6
MarinaRT2003 <mreptru297@g.educaand.es>
2025-02-10 10:56:09 +0100

    10/02 add codigo

--- calculadora.js
+++ calculadora.js
@@ -1,23 +1,43 @@
 // calculadora.js
 
-function sumar(a, b) {
+// function suma(a, b) {
+//     return a + b;
+// }
+  
+// function resta(a, b) {
+//     return a - b;
+// }
+
+// function multiplica(a, b) {
+//     return a * b;
+// }
+
+// function dividi(a, b) {
+//     return a / b;
+// }   
+
+// function raizCuadrada(a) {
+//     return Math.sqrt(a);
+// }
+  
+// module.exports = { suma, resta, multiplica, dividi, raizCuadrada };
+
+function suma(a, b) {
     return a + b;
-}
-  
-function restar(a, b) {
+   }
+   function resta(a, b) {
     return a - b;
-}
-
-function multiplicar(a, b) {
+   }
+   function multiplicar(a, b) {
     return a * b;
-}
-
-function dividir(a, b) {
+   }
+   function dividir(a, b) {
+    if (b === 0) {
+    throw new Error("No se puede dividir por cero");
+    }
     return a / b;
-}   
-
-function raizCuadrada(a) {
-    return Math.sqrt(a);
-}
-  
-module.exports = { sumar, restar, multiplicar, dividir, raizCuadrada };
+   }
+   // Hacer las funciones accesibles en el navegador
+   window.suma = suma;
+   window.resta = resta;
+   window.multiplicar = multip
